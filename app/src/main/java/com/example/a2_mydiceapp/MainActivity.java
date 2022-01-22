aebc9784e88e876943a88fa0f57a2e0165b58910
mortessa99 <smortezajahan@gmail.com>
2022-01-23 00:28:05 +0330

    end of less 17

--- app/src/main/java/com/example/a2_mydiceapp/MainActivity.java
+++ app/src/main/java/com/example/a2_mydiceapp/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.a2_mydiceapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnRollDice;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnRollDice = findViewById(R.id.btnRollDice);
+        btnRollDice.setOnClickListener(view -> {
+            int random = new Random().nextInt(7);
+            Toast.makeText(this,String.valueOf(random),Toast.LENGTH_SHORT).show();
+        });
     }
 }
