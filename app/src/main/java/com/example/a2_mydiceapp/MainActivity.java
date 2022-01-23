fe079a84bfb1d0911e61a6f848e822ef5c065120
mortessa99 <smortezajahan@gmail.com>
2022-01-24 01:20:11 +0330

    end lesson 19- add animation and sound for dice app

--- app/src/main/java/com/example/a2_mydiceapp/MainActivity.java
+++ app/src/main/java/com/example/a2_mydiceapp/MainActivity.java
@@ -3,16 +3,21 @@ package com.example.a2_mydiceapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Dialog;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     Button btnRollDice;
     ImageView imgDice1,imgDice2;
+    MediaPlayer mediaPlayer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
         btnRollDice = findViewById(R.id.btnRollDice);
         imgDice1 = findViewById(R.id.imgDice1);
         imgDice2 = findViewById(R.id.imgDice2);
+        mediaPlayer = MediaPlayer.create(this,R.raw.dice_sound);
 
         int[] diceImages = {
                 R.drawable.dice1,
@@ -39,11 +45,26 @@ public class MainActivity extends AppCompatActivity {
             //Toast.makeText(this,String.valueOf(myRandom),Toast.LENGTH_SHORT).show();
 
 
-            int random1 = new Random().nextInt(6) ;
-            imgDice1.setImageResource(diceImages[random1]);
+            int random = new Random().nextInt(6) ;
+            imgDice1.setImageResource(diceImages[random]);
 
-            int random2 = new Random().nextInt(6);
-            imgDice2.setImageResource(diceImages[random2]);
+            // or create an other variable like random for example int randomTwo = new Random().nextInt(6) ;
+            // and use it in imgDice2.setImageResource(diceImages[randomTwo]);
+
+            random = new Random().nextInt(6);
+            imgDice2.setImageResource(diceImages[random]);
+
+            YoYo.with(Techniques.Tada)
+                    .duration(700)
+                    .repeat(0)
+                    .playOn(imgDice1);
+
+            YoYo.with(Techniques.Shake)
+                    .duration(700)
+                    .repeat(0)
+                    .playOn(imgDice2);
+
+            mediaPlayer.start();
         });
     }
 }
